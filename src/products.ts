5fab03fa664faad1386aaf1e15bf44944a458e9b
manavjethaloja-glitch <manav.jethaloja@gdgoenkasurat.com>
2026-05-18 01:27:44 +0530

    Create products.ts

---
+++ src/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    id: 1,
+    name: "Black Hoodie",
+    price: 2499,
+    image: "/images/hoodie.jpg",
+    category: "hoodie",
+  },
+
+  {
+    id: 2,
+    name: "Oversized Tee",
+    price: 1499,
+    image: "/images/tshirt.jpg",
+    category: "tshirt",
+  },
+];
