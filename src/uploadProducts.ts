a63350be1e91d3e0747204643bd1fe2c05e5f871
manavjethaloja-glitch <manav.jethaloja@gdgoenkasurat.com>
2026-05-18 01:28:26 +0530

    Update uploadProducts.ts

--- src/uploadProducts.ts
+++ src/uploadProducts.ts
@@ -1,1 +1,16 @@
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { products } from "./products";
 
+export const uploadProducts = async () => {
+  try {
+    for (const product of products) {
+      await addDoc(collection(db, "products"), product);
+    }
+
+    console.log("Products uploaded!");
+    alert("Products uploaded successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+};
