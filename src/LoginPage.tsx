0adaafd7983512d0a99d33f12a84b6ca526ad7b2
manavjethaloja-glitch <manav.jethaloja@gdgoenkasurat.com>
2026-05-18 13:38:40 +0530

    Update LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import {
   signInWithPopup,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
 } from "firebase/auth";
 
 import { auth, googleProvider } from "./firebase";
