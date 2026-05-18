b39b90f4a6bd1446161ea830f31ffdbcc8e66696
manavjethaloja-glitch <manav.jethaloja@gdgoenkasurat.com>
2026-05-18 13:28:10 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOgPyYAIuP-txKO4-4tPdh3tZ08bJ74J4",
@@ -14,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
