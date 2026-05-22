01fd8d78bb405a63235df59c62df56ea7bdbf20b
Blank-space <manavjethaloja@icloud.com>
2026-05-22 21:13:22 +0530

    Delete src/pages/Home.tsx

--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-green-500 text-white flex items-center justify-center text-4xl">
-      HOME FILE WORKING
-    </div>
-  );
-}
